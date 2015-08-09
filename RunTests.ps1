98e6b98bb590c16e9c0b6dd534a0eda8a88def81
pocketberserker <pocketberserker@gmail.com>
2015-08-09 21:23:46 +0900

    update Persimmon.Console

--- RunTests.ps1
+++ RunTests.ps1
@@ -2,4 +2,4 @@ $inputs = @(
   ".\PEGStudy.Tests\bin\Debug\PEGStudy.Tests.dll"
 )
 
-.\packages\Persimmon.Console.1.0.0-beta6\tools\Persimmon.Console.exe --parallel $inputs
+.\packages\Persimmon.Console.1.0.0-beta8\tools\Persimmon.Console.exe --parallel $inputs
